a8235bc9abd480c0e96bfabd97ac29131fca1f6e
Andriy Sultanov <as.andreysultanov@gmail.com>
2020-02-16 16:03:16 +0200

    2

--- main.py
+++ main.py
@@ -11,19 +11,33 @@ def read_file(path):
     Reads the file and adds all the
     '''
     print("HUY")
+    # Setting up geopy
     geolocator = Nominatim(user_agent="LastGenius", timeout=1)
     geocode = RateLimiter(geolocator.geocode, min_delay_seconds=0.01)
+
+    print("HUY1")
+    # Reading the file
     data = pandas.read_csv(path, error_bad_lines=False, warn_bad_lines=False)
     movie = data['movie']
     year = data['year']
     location = data['location']
     movie_data = zip(movie, year, location)
+
+    used_locations_dict = dict()
+
+    print("HUY2")
     with open('locations_final.csv', "w") as file:
         for movie in movie_data:
             try:
-                coordinates = geolocator.geocode(movie[2].strip("/n"))
-                print(movie, coordinates.latitude, coordinates.longitude)
-                file.write(','.join([movie[0], movie[1], movie[2], coordinates.latitude, coordinates.longitude]))
+                location_name = movie[2].strip("/n")
+                if location_name in used_locations_dict:
+                    coordinates = used_locations_dict[location_name]
+                else:
+                    coordinates = geolocator.geocode(location_name)
+                    coordinates = (coordinates.latitude, coordinates.longitude)
+                    used_locations_dict[location_name] = coordinates
+                print(movie, coordinates[0], coordinates[1])
+                file.write(','.join([movie[0], movie[1], location_name, coordinates[0], coordinates[1]]))
             except:
                 continue
 
@@ -32,11 +46,11 @@ def main():
     # year = input("Please enter a year you would like to have a map for: ")
     # lat, long = map((lambda x: float(x)), input("Please enter your location (format: lat, long): ").split(", "))
     path = "locations.csv"
-    map = folium.Map()
+    # map = folium.Map()
     read_file(path)
     # location_data = read_location(movies)
     # print(location_data)
-    map.save("map.html")
+    # map.save("map.html")
 
 
 if __name__ == '__main__':
