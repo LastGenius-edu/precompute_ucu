11bcc37e34ac6ef5c1feaeae1fb2a0d447ac7c6d
Andriy Sultanov <as.andreysultanov@gmail.com>
2020-02-16 16:26:56 +0200

    3

--- main.py
+++ main.py
@@ -10,12 +10,10 @@ def read_file(path):
 
     Reads the file and adds all the
     '''
-    print("HUY")
     # Setting up geopy
     geolocator = Nominatim(user_agent="LastGenius", timeout=1)
     geocode = RateLimiter(geolocator.geocode, min_delay_seconds=0.01)
 
-    print("HUY1")
     # Reading the file
     data = pandas.read_csv(path, error_bad_lines=False, warn_bad_lines=False)
     movie = data['movie']
@@ -25,7 +23,6 @@ def read_file(path):
 
     used_locations_dict = dict()
 
-    print("HUY2")
     with open('locations_final.csv', "w") as file:
         for movie in movie_data:
             try:
@@ -36,7 +33,8 @@ def read_file(path):
                     coordinates = geolocator.geocode(location_name)
                     coordinates = (coordinates.latitude, coordinates.longitude)
                     used_locations_dict[location_name] = coordinates
-                print(movie, coordinates[0], coordinates[1])
+                with open("tmp.txt", "w") as tmp:
+                    tmp.write(','.join([movie, coordinates[0], coordinates[1]]))
                 file.write(','.join([movie[0], movie[1], location_name, coordinates[0], coordinates[1]]))
             except:
                 continue
@@ -55,4 +53,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
